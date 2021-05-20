062e1ffc22e4140ef4ad6478a7827bfedcba4e21
BelongaRobert <68036456+BelongaRobert@users.noreply.github.com>
2021-05-20 16:35:38 -0400

    RB: Trying to get java Project uploaded

---
+++ Garage.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+
+public class Garage {
+    
+    protected List<Vehicle> garageList;
+
+    public Garage(List<Vehicle> garageList){
+        this.garageList = garageList;
+    }
+
+    public void addVehicle()
+
+}
